b9d6043dc804b52e1b710b873569b10efbb8bd34
MLin27 <40908837+MLin27@users.noreply.github.com>
2022-03-07 12:29:07 +0100

    minor update
    Roles localized, autofill roles for the process

--- modules/web/src/com/company/bpmapp/web/screens/outgoingdocument/OutgoingDocumentEditAction.java
+++ modules/web/src/com/company/bpmapp/web/screens/outgoingdocument/OutgoingDocumentEditAction.java
@@ -1,5 +1,17 @@
 package com.company.bpmapp.web.screens.outgoingdocument;
 
+import com.haulmont.bpm.entity.ProcActor;
+import com.haulmont.bpm.entity.ProcInstance;
+import com.haulmont.bpm.entity.ProcRole;
+import com.haulmont.bpm.gui.action.StartProcessAction;
+import com.haulmont.bpm.service.BpmEntitiesService;
+import com.haulmont.bpm.service.ProcessRuntimeService;
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.core.global.View;
+import com.haulmont.cuba.gui.Notifications;
+import com.haulmont.cuba.gui.components.Button;
+import com.haulmont.cuba.gui.components.GroupBoxLayout;
+import com.haulmont.cuba.gui.components.HBoxLayout;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.bpmapp.entity.OutgoingDocument;
 import com.haulmont.bpm.entity.ProcAttachment;
@@ -10,8 +22,16 @@ import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.model.InstanceLoader;
 import com.haulmont.cuba.gui.screen.*;
+import com.haulmont.cuba.gui.util.OperationResult;
+import com.haulmont.cuba.security.entity.User;
+import com.haulmont.cuba.security.global.UserSession;
+import sun.misc.resources.Messages;
 
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 
 @UiController("bpmapp_OutgoingDocument.editAction")
@@ -20,10 +40,7 @@ import javax.inject.Inject;
 @LoadDataBeforeShow
 public class OutgoingDocumentEditAction extends StandardEditor<OutgoingDocument> {
     private static final String PROCESS_CODE = "approvalOfTheOutgoingDocument";
-
-    @Inject
-    private CollectionLoader<ProcAttachment> procAttachmentsDl;
-
+    
     @Inject
     private InstanceContainer<OutgoingDocument> outgoingDocumentDc;
 
@@ -31,20 +48,71 @@ public class OutgoingDocumentEditAction extends StandardEditor<OutgoingDocument>
     protected ProcActionsFragment procActionsFragment;
 
     @Inject
-    private Table<ProcAttachment> attachmentsTable;
+    private InstanceLoader<OutgoingDocument> outgoingDocumentDl;
 
     @Inject
-    private InstanceLoader<OutgoingDocument> outgoingDocumentDl;
+    private BpmEntitiesService bpmEntitiesService;
+
+    @Inject
+    private Metadata metadata;
+
 
     @Subscribe
     private void onBeforeShow(BeforeShowEvent event) {
-        outgoingDocumentDl.load();
-        procAttachmentsDl.setParameter("entityId",outgoingDocumentDc.getItem().getId());
-        procAttachmentsDl.load();
+        initProcActionsFragment();
+        changeStartProcessBtnCaption();
+    }
+
+    private void initProcActionsFragment() {
         procActionsFragment.initializer()
                 .standard()
-                .init(PROCESS_CODE, getEditedEntity());
+                .setBeforeStartProcessPredicate(() -> {
+                    if (commitChanges().getStatus() == OperationResult.Status.SUCCESS) {
+                        ProcInstance procInstance = procActionsFragment.getProcInstance();
+                        ProcActor initiatorProcActor = createProcActor("initiator", procInstance, getEditedEntity().getExecutor().getUser());
+                        ProcActor headOfDepProcActor = createProcActor("headOfDep", procInstance, getEditedEntity().getExecutor().getSubdivision().getHeadOfDepartment().getUser());
+                        ProcActor signatoryProcActor = createProcActor("signatory", procInstance, getEditedEntity().getSignatory().getUser());
+                        Set<ProcActor> procActors = new HashSet<>();
+                        procActors.add(initiatorProcActor);
+                        procActors.add(headOfDepProcActor);
+                        procActors.add(signatoryProcActor);
+                        procInstance.setProcActors(procActors);
+                        return true;
+                    }
+                    return false;
+                })
+                .setStartProcessActionProcessVariablesSupplier(() -> {
+                    Map<String, Object> processVariables = new HashMap<>();
+                    processVariables.put("state", getEditedEntity().getState());
+                    return processVariables;
+                })
+                .setAfterStartProcessListener(() -> {
+                    initProcActionsFragment();
+                    outgoingDocumentDl.setEntityId(getEditedEntity().getId());
+                    outgoingDocumentDl.load();
+                })
+                .setAfterCompleteTaskListener(() -> {
 
-        FileDownloadHelper.initGeneratedColumn(attachmentsTable, "file");
+                    initProcActionsFragment();
+                    outgoingDocumentDl.setEntityId(getEditedEntity().getId());
+                    outgoingDocumentDl.load();
+                })
+                .init(PROCESS_CODE, getEditedEntity());
+    }
+
+    private ProcActor createProcActor(String procRoleCode, ProcInstance procInstance, User user) {
+        ProcActor initiatorProcActor = metadata.create(ProcActor.class);
+        initiatorProcActor.setUser(user);
+        ProcRole initiatorProcRole = bpmEntitiesService.findProcRole(PROCESS_CODE, procRoleCode, View.MINIMAL);
+        initiatorProcActor.setProcRole(initiatorProcRole);
+        initiatorProcActor.setProcInstance(procInstance);
+        return initiatorProcActor;
+    }
+
+    private void changeStartProcessBtnCaption() {
+        StartProcessAction startProcessAction = procActionsFragment.getStartProcessAction();
+        if (startProcessAction != null) {
+            startProcessAction.setCaption("Start process");
+        }
     }
 }
